8f63fd9f66a44ce31c4d85fb71e607e846ae34e6
Stefano Bernardo <stefano.berna8@gmail.com>
2023-11-13 18:25:05 +0100

    Rev. 3

--- script.js
+++ script.js
@@ -12,14 +12,8 @@ REGOLE
 */
 
 /*
-    Supereroi?
-    Immagina 5 supereroi, ognuno con poteri diversi e ognuno comunica in maniera diversa.
-
-    Numb3r: lui é il supereroe dei numeri, grazie ai suoi poteri puó fare calcoli velocissimi e ricordarsi un sacco di numeri, anche lunghissimi!
-    "string": lui é un supereroe molto prolisso, il suo superpotere é confondere tutti con le sue parole e i suoi discorsi, le sa tutte anche perché puó contenere qualsiasi tipo di carattere o cifra
-    boolean: lui é il supereroe della veritá, per lui il mondo é solo bianco e nero e esiste solo ció che é VERO e ció che é FALSO
-    null: 
-    uNd3fiNed: lui é il cattivo, é il caos in persona e non fa altro che creare scompiglio
+    
+    Vedi sotto e sito allegato.
 
 */
 
@@ -27,39 +21,39 @@ REGOLE
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-let sum = 12 + 20;
-console.log("La somma tra 12 e 20 é " + sum);
+/*let sum = 12 + 20;
+console.log("La somma tra 12 e 20 é " + sum);*/
 
 /* ESERCIZIO 3
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
-let x = 12;
-console.log("X é pari a " + x);
+/*let x = 12;
+console.log("X é pari a " + x);*/
 
 /* ESERCIZIO 4
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-let name = "Stefano";
-console.log("Il mio nome é " + name);
+// let name = "Stefano";
+// console.log("Il mio nome é " + name);
 
 /* ESERCIZIO 5
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-console.log( 4 - x );
+/*console.log( 4 - x );*/
 
 /* ESERCIZIO 6
  Crea un oggetto che ti rappresenti.
 */
 
-let person = {
+/*let person = {
     name: "Stefano",
     surname : "Bernardo",
     hobby: "Photography"
 };
-console.log(person);
+console.log(person);*/
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -68,6 +62,56 @@ console.log(person);
  NON HAI BISOGNO DI UN BLOCCO IF/ELSE. E' sufficiente utilizzare console.log().
 */
 
-let name1 = "john";
+/*let name1 = "john";
 let name2 = "John";
-console.log(name1 === name2.toLowerCase());
+console.log(name1 === name2.toLowerCase());*/
+
+
+/* codice buttons*/
+
+function sumFunction() {
+    let a = prompt("Dammi il primo numero");
+    let b = prompt("Dammi il secondo numero");
+    a = parseFloat(a);
+    b = parseFloat(b);
+    sum = a + b;
+    window.alert("La somma di " + a + " e " + b + " é " + sum);
+}
+
+function myPhrase() {
+    let phrase1 = prompt("Scrivi la prima parte della frase...");
+    let phrase2 = prompt("Scrivi la seconda parte della frase...");
+    window.alert(phrase1 + " " + phrase2);
+}
+
+function checkFunction() {
+    let phrase1 = prompt("Prova a scrivere una parola, numero o frase");
+    let phrase2 = prompt("Riscrivi la stessa cosa scritta prima");
+    window.alert(phrase1 === phrase2);
+}
+
+function makeNull() {
+    let a = prompt("Scrivi qualsiasi cosa:");
+    let check = confirm("Vuoi rendere la variabile nulla?");
+    if (check) {
+        a = null;
+        window.alert("Il valore di a adesso é " + a);
+    } else {
+        window.alert("Il valore di a é rimasto " + a);
+    }
+}
+
+function undefinedFunction() {
+    let a = prompt("Prova a dirle qualcosa...");
+    let b;
+    window.alert("Io sono " + b);
+}
+
+function myObject() {
+    let object = {
+        name : prompt("Dimmi il tuo nome"),
+        surname : prompt("Dimmi il tuo cognome"),
+        age : prompt("Quanti anni hai?")
+    }
+    window.alert("Il tuo nome é " + object.name + ", il tuo cognome é " + object.surname + " e hai " + object.age + " anni!");
+}
